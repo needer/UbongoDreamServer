f0f025294b5036dc2709504055abceb2aa7b35cf
AlbertHambardzumyan <hambardzumyan.albert@gmail.com>
2016-04-18 11:41:16 +0200

    Main activity: establish connection & test

--- Client_Server_Communication/src/com/ubongo/UbonGo.java
+++ Client_Server_Communication/src/com/ubongo/UbonGo.java
@@ -7,9 +7,6 @@ import com.example.UbonGo.R;
 import com.ubongo.serverManager.ServerManager;
 
 public class UbonGo extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
